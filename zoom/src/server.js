d1fa2dc8433beddd69b29f98664e2b4a266b39a0
42sangkkim <sangkkim@student.42seoul.kr>
2023-07-09 19:41:11 +0900

    Feat: chat with websockets #1

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -1,4 +1,8 @@
+import http from "http";
 import express from "express";
+import { WebSocketServer } from 'ws';
+import { on } from "events";
+import { type } from "os";
 
 const app = express();
 
@@ -6,8 +10,37 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+function onSocketClose() {
+  console.log("Disconnected from the Browser ❌");
+}
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  // console.log(socket);
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+  console.log("Connected to Browser ✅");
+  socket.on("close", onSocketClose);
+  socket.on("message", (msg) => {
+  //   console.log(message.toString());
+    const message = JSON.parse(msg.toString());
+    switch(message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
